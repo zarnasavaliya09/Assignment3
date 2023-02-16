d8b4afad3753c696c5a2bb06407330ccd1900605
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-02-16 10:12:10 -0500

    create function and delay response usingsetTimeout

--- js/script.js
+++ js/script.js
@@ -1,28 +1,58 @@
-$(document).ready(function() {
-    var myItems;
-
-    $.getJSON('team.json', function(data) {
-        myItems = data.items;
-        console.log(myItems);
-        const teamContainer = document.querySelector('#team');
-        $.each(data,function(key,value){
-
-            var personName = document.createElement('h2');
-            personName.textContent = value.name;
-
-            var personPos = document.createElement('h5');
-            personPos.textContent = value.position;
-
-            var personBio = document.createElement('p');
-            personBio.textContent = value.bio;
-
-            var personDiv =document.createElement("div");
-
-            const memberDiv = $("<div>").append(memberName, memberPosition, memberBio);
-            $("#team").append(memberDiv);
-
-
+function fetchJson() {
+    $("#team").text("Loading...");
+    $.getJSON("team.json", function (data) {
+      setTimeout(function () {
+        $("#team").empty();
+        $.each(data, function (key, value) {
+          var personName = document.createElement("h2");
+          personName.textContent = value.name;
+  
+          var personPos = document.createElement("h5");
+          personPos.textContent = value.position;
+  
+          var personBio = document.createElement("p");
+          personBio.textContent = value.bio;
+  
+          const memberDiv = $("<div>").append(personName, personPos, personBio);
+          $("#team").append(memberDiv);
         });
-
+      }, 3000);
     });
-});
+  }
+  function fetchAjax() {
+    $.ajax({
+      method: "GET",
+      url: "team.json",
+      dataType: "JSON",
+      beforeSend: function () {
+        // Display "Loading..." message in #team div
+        $("#team").text("Loading...");
+      },
+      success: function (data, value) {
+        setTimeout(function () {
+          $("#team").empty();
+          $.each(data, function (key, value) {
+            var personName = document.createElement("h2");
+            personName.textContent = value.name;
+  
+            var personPos = document.createElement("h5");
+            personPos.textContent = value.position;
+  
+            var personBio = document.createElement("p");
+            personBio.textContent = value.bio;
+  
+            const memberDiv = $("<div>").append(personName, personPos, personBio);
+            $("#team").append(memberDiv);
+          });
+        }, 3000);
+      },
+      error: function (error) {
+        $("#team").append("The content could not be retrieved");
+      }
+    });
+  }
+  $(document).ready(function () {
+    //fetchJson();
+    fetchAjax();
+  });
+  
